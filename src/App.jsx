26391027f3250c68792c21b44f6b8bbc4c6d7e3c
boilerrat <128boilerrat@gmail.com>
2023-12-02 14:29:52 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useEffect } from 'react'
+import './App.css'
+
+function App() {
+  useEffect(() => {
+    if (window.Tally) {
+      window.Tally.loadEmbeds();
+     }
+    }, 
+  []
+ );
+
+// Inside your App function
+return (
+  <>
+    <h1 className="registration-form">
+      Registration Form
+    </h1>  
+  
+    <iframe 
+      data-tally-src="https://tally.so/embed/wLPkGy?alignLeft=1&hideTitle=0&transparentBackground=0&dynamicHeight=1"
+      loading="lazy" 
+      width="650em" 
+      height="200" 
+//      frameborder="0" 
+//      marginheight="0" 
+//      marginwidth="0" 
+      title="Registration Form"
+    ></iframe>
+  </>
+);
+}
+
+export default App;
+
