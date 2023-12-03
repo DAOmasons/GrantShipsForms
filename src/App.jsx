e88b06657d68c48bee70817c4dfd3ca4d0af3edb
boilerrat <128boilerrat@gmail.com>
2023-12-02 21:24:25 -0500

    appbar material ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 import { useEffect } from 'react'
 import './App.css'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 function App() {
   useEffect(() => {
@@ -12,7 +20,20 @@ function App() {
 
 // Inside your App function
 return (
-  <>
+    <>
+     <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static" sx={{ bgcolor: 'grey.900', color: 'white' }}>
+        <Toolbar>
+          <Typography>
+
+          </Typography>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            
+          </Typography>
+          <Button color="inherit" component="a" href="https://rules.grantship.fun" target="_blank">Rule Book</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
     <h1 className="registration-form">
       Grant Ships Registration
     </h1>  
